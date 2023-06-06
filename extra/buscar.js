cca8ce220bc15741c816dbe681796f8c3c86870a
MPasti <matheusspasti@gmail.com>
2023-06-06 11:53:00 -0300

    Atualizações no sistema

--- extra/buscar.js
+++ extra/buscar.js
@@ -1,3 +1,4 @@
 console.log(document.querySelector('input[value="2"]'));
 console.log(document.querySelector('input[type="tel"]'));
 //para buscar o elemento preciso com o querySelector
+console.log(document.querySelectorAll('input[type="button"]'));
--- main.js
+++ main.js
@@ -2,16 +2,23 @@ function batida() {
   document.querySelector("#som_tecla_pom").play();
   //código esta guardando entre parenteses para ser exectado apenas com a execução
 }
+
+const listaTeclas = document.querySelectorAll(".tecla");
+//isto é uma referencia, o valor é constante pois não vai mudar
+
+listaTeclas[0].onclick = batida;
+
 //o navegador bloqueia funções que executa midias antes do usuario interagir com a página
 //assim, precisamos do .play estar dentro de algo que o chama com interação do usuário, como uma função
 
-alert(`pom`);
-document.querySelector(".tecla_pom").onclick = batida;
+//document.querySelector(".tecla_pom").onclick = batida;
 
-//não é o ideal usar o onclick ou qualquer outra função direto do html
-//se queremos guardar uma função em um onclick, precisamos tirar o parenteses da função
-//caos contrário irá executar na hora, isso é por causa da ordem de execução do JS
+/*
+não é o ideal usar o onclick ou qualquer outra função direto do html
+se queremos guardar uma função em um onclick, precisamos tirar o parenteses da função
+caos contrário irá executar na hora, isso é por causa da ordem de execução do JS
 
-//sem o parenteses vai guardar a referencia da função, e não sua execução ()
+sem o parenteses vai guardar a referencia da função, e não sua execução ()
 
-//ao o onclick ser executado, ele quer que retorne o som
+ao o onclick ser executado, ele quer que retorne o som
+*/
