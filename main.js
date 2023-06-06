bba738d9c442eb6e15c78617e755bfb9e097da84
MPasti <matheusspasti@gmail.com>
2023-06-06 11:00:18 -0300

    Adicionando funções

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 function batida() {
-  alert(`pom`);
-  console.log(document.querySelector(".tecla_pom"));
+  document.querySelector("#som_tecla_pom").play();
+  //código esta guardando entre parenteses para ser exectado apenas com a execução
 }
+//o navegador bloqueia funções que executa midias antes do usuario interagir com a página
+//assim, precisamos do .play estar dentro de algo que o chama com interação do usuário, como uma função
+
+alert(`pom`);
+document.querySelector(".tecla_pom").onclick = batida;
+
+//não é o ideal usar o onclick ou qualquer outra função direto do html
+//se queremos guardar uma função em um onclick, precisamos tirar o parenteses da função
+//caos contrário irá executar na hora, isso é por causa da ordem de execução do JS
+
+//sem o parenteses vai guardar a referencia da função, e não sua execução ()
+
+//ao o onclick ser executado, ele quer que retorne o som
